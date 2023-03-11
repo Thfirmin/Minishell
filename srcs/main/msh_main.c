6f02e5c4d7777d80a858c8abe18cd7e124295890
Thfirmin <thfirmin.dsantos@gmail.com>
2023-03-11 01:50:51 -0300

    Get and init environment variable complete

--- srcs/main/msh_main.c
+++ srcs/main/msh_main.c
@@ -6,11 +6,13 @@
 /*   By: thfirmin <thfirmin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/07 23:53:26 by thfirmin          #+#    #+#             */
-/*   Updated: 2023/03/10 02:35:54 by thfirmin         ###   ########.fr       */
+/*   Updated: 2023/03/11 01:49:59 by thfirmin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+static void	msh_printenv(t_env *env);
 
 int	main(int argc, char *argv[], char *envp[])
 {
@@ -18,9 +20,17 @@ int	main(int argc, char *argv[], char *envp[])
 	
 	(void) argc;
 	(void) argv;
-	(void) env;
 	env = msh_initenv(envp);
-	//printf ("prompt = %s\n", prompt);
+	msh_printenv(env);
 	msh_envclear(&env);
 	return (0);
+}
+
+static void	msh_printenv(t_env *env)
+{
+	while (env)
+	{
+		ft_printf ("%s=%s\n", env->key, env->vlue);
+		env = env->next;
+	}
 }
