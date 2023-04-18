bc713bf628ed9a4aa8618f663d0fa29d2837c796
thfirmin <thiagofirmino2001@gmail.com>
2023-04-18 08:44:17 -0300

    Feature:Split-line-per-pipe

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: thfirmin <thfirmin@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/14 02:05:18 by thfirmin          #+#    #+#             */
-/*   Updated: 2023/04/18 00:59:26 by thfirmin         ###   ########.fr       */
+/*   Updated: 2023/04/18 08:20:56 by thfirmin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,6 +36,13 @@
 // Enums
 
 // Structs & Unions
+typedef struct s_cmd
+{
+	int				fdin;
+	int				fdout;
+	char			**args;
+	struct s_cmd	*next;
+}					t_cmd;
 
 // Test
 
@@ -48,5 +55,9 @@ int	msh_skipquote(char *str);
 
 // Lexer
 int	msh_lexer(char *line);
+
+// Parser
+t_cmd	*msh_parser(char *line);
+char	**msh_pipe_split(char *line);
 
 #endif
