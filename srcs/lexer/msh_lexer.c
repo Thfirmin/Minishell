e2bb081597c1b02c5a7b304cedb6040d3ff92f05
thfirmin <thiagofirmino2001@gmail.com>
2023-04-18 01:04:29 -0300

    completed lexer

---
+++ srcs/lexer/msh_lexer.c
@@ -0,0 +1,98 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   msh_lexer.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: thfirmin <thfirmin@student.42.rio>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/18 00:53:19 by thfirmin          #+#    #+#             */
+/*   Updated: 2023/04/18 00:55:37 by thfirmin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+static int		msh_lexerror(char c);
+
+static short	msh_redirsyntax(char *str, char opt);
+
+static short	msh_pipesyntax(char *str);
+
+// Lexer algorithm: analisys of prompt syntax
+int	msh_lexer(char *line)
+{
+	if (!msh_pipesyntax(line))
+		return (0);
+	else if (!msh_redirsyntax(line, '<'))
+		return (0);
+	else if (!msh_redirsyntax(line, '>'))
+		return (0);
+	return (1);
+}
+
+// Verify syntax of pipe operator (non-portable)
+static short	msh_pipesyntax(char *str)
+{
+	short	arg;
+
+	arg = 0;
+	while (*str)
+	{
+		str += msh_skipquote(str);
+		if (!*str)
+			return (msh_lexerror(*str));
+		if (!arg && (!ft_isspace(*str) && (*str != '|')))
+			arg = '|';
+		if (*str == '|')
+		{
+			if (!arg)
+				return (msh_lexerror(*str));
+			arg -= *str;
+		}
+		str ++;
+	}
+	if (!arg)
+		return (msh_lexerror(*str));
+	return (arg);
+}
+
+// Verify syntax of redirects operators (non-portable)
+static short	msh_redirsyntax(char *str, char opt)
+{
+	short	redir;
+
+	redir = 0;
+	while (*str)
+	{
+		str += msh_skipquote(str);
+		if (!*str)
+			return (msh_lexerror(*str));
+		if (*str == opt)
+		{
+			while (*(str + redir) == opt)
+				redir ++;
+			if (redir > 2)
+				return (msh_lexerror(*str));
+			str += redir;
+			while (ft_isspace(*str))
+				str ++;
+			if (!*str || *str == '|' || *str == '>' || *str == '<')
+				return (msh_lexerror(*str));
+		}
+		redir = 0;
+		str ++;
+	}
+	return (1);
+}
+
+// Display error message (change to unique function)
+static int	msh_lexerror(char c)
+{
+	ft_putstr_fd("-bash: syntax error near unexpected token \'", 2);
+	if (!c)
+		ft_putstr_fd("newline", 2);
+	else
+		ft_putchar_fd(c, 2);
+	ft_putendl_fd("\'", 2);
+	return (0);
+}
---
+++ srcs/utils/msh_skipquote.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   msh_skipquote.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: thfirmin <thfirmin@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/16 10:24:02 by thfirmin          #+#    #+#             */
+/*   Updated: 2023/04/16 22:52:40 by thfirmin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+// Verify if string's a quote (double/single) and return the quote field len
+int		msh_skipquote(char *str)
+{
+	int		len;
+	char	opt;
+
+	if (!str)
+		return (0);
+	len = 0;
+	if ((*(str + len) == '\'') || (*(str + len) == '\"'))
+	{
+		opt = *str++;
+		while (*(str + len) && (*(str + len) != opt))
+			len ++;
+	}
+	return (len);
+}
