5297b0c053b2a676d1edecab6d76823e7ec06c88
thfirmin <thiagofirmino2001@gmail.com>
2023-04-17 23:04:30 -0300

    reset develop to re-structure

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: thfirmin <thfirmin@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/14 02:05:18 by thfirmin          #+#    #+#             */
-/*   Updated: 2023/04/15 16:33:10 by thfirmin         ###   ########.fr       */
+/*   Updated: 2023/04/17 23:03:15 by thfirmin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,31 +16,31 @@
 // Defines
 
 // Includes
-# include "libft.h"
 # include <readline/readline.h>
+# include <readline/history.h>
+# include <sys/wait.h>
+# include <sys/stat.h>
+# include <sys/ioctl.h>
+# include <dirent.h>
+# include <temrios.h>
+# include <curses.h>
+# include <term.h>
+# include <string.h>
+# include <signal.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <fcntl.h>
+# include "libft.h"
 # include <errno.h>
 
 // Enums
 
 // Structs & Unions
-typedef struct s_cmd
-{
-	char			**arg;
-	int				fdin;
-	int				fdout;
-	struct s_cmd	*next;
-}					t_cmd;
 
 // Test
-void	test(void);
 
 // Prompt
-t_cmd	*msh_prompt(void);
-int		msh_lexer(char *line);
-t_cmd	*msh_parser(char *line);
 
 // Data
-t_cmd	*msh_cmdnew(char **arg, int fdin, int fdout);
-void	msh_cmdadd_back(t_cmd **cmd, t_cmd *arg);
 
 #endif
--- srcs/main/minishell.c
+++ srcs/main/minishell.c
@@ -6,7 +6,7 @@
 /*   By: thfirmin <thfirmin@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/14 03:34:40 by thfirmin          #+#    #+#             */
-/*   Updated: 2023/04/14 21:19:38 by thfirmin         ###   ########.fr       */
+/*   Updated: 2023/04/17 23:03:28 by thfirmin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,5 @@ int	main(int argc, char *argv[], char *envp[])
 	(void) argc;
 	(void) argv;
 	(void) envp;
-	test();
 	return (0);
 }
